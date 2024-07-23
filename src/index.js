2234ed15fddb484ce388832ff316107a01936ac4
노의빈 <hosan_1@naver.com>
2024-07-23 13:48:30 +0900

    test: 테스트 코드 수정

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ root.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
-
+// test code
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
